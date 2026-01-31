f34d1cf7eab4bd6795aef30d9b62a6e156aa9d9e
chatbotluckteesid <alyanurrakhman@gmail.com>
2026-01-31 04:59:35 +0000

    commit

--- web_app.py
+++ web_app.py
@@ -1,24 +1,17 @@
 from flask import Flask, render_template, request, jsonify
-from core import get_response
+from core import get_bot_reply
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-
-    # Validasi data
-    if not data or "message" not in data:
-        return jsonify({"reply": "Pesan tidak boleh kosong"}), 400
-
-    message = data["message"]
-    reply = get_response(message, user_id="web_user")  # user_id tetap untuk web
-
+    reply = get_bot_reply(data.get("message",""))
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
